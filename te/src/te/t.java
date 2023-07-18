93abe07ce14d3f555a8e06232f59f9bc75da582e
victoai <117916629+victoai@users.noreply.github.com>
2023-07-18 23:25:59 +0900

    Update t.java
    ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

--- te/src/te/t.java
+++ te/src/te/t.java
@@ -2,7 +2,7 @@ package te;
 
 public class t {
 	public static void main(String[] args) {
-		System.out.println("djjfddfdfcvcvcvcvcvcvcvcdfdfjfjf");
+		System.out.println("djjfddfdfcvcvcvcvcvcvcvcdfdfjfjf  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11");
 	}
 
 }
